dc1c1b73d20253d0064d3c52182f4ec77e99fbe1
Ashutosh <ashutoshforjobs@gmail.com>
2021-05-24 01:26:12 +0530

    removed duplicate git

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { colors } from "../theme";
+import { FullHeightGrid } from "./Common";
+
+const FooterWrapper = styled.div`
+  background-color: ${colors.primary};
+  padding: 1vw 4vw;
+`;
+
+const FooterText = styled.p`
+  color: #fff;
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterWrapper>
+      <FullHeightGrid container justify="center" alignItems="center">
+        <FooterText>Copyright Singh Classes</FooterText>
+      </FullHeightGrid>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
